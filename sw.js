ed210b8d125406d3a78b09e36e72e666e4a1cca0
Saranpat Boeploy <nettolimited@MacBook-Pro-khxng-Saranpat.local>
2026-06-11 12:13:20 +0700

    fix: update sw cache version to v57

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'finance-v56';
+const CACHE = 'finance-v57';
 const FILES = ['./index.html', './style.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
