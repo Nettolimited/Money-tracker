3a659638ce228601a8b9ba70243bfd09e9e1f064
Saranpat Boeploy <nettolimited@MacBook-Pro-khxng-Saranpat.local>
2026-06-11 12:26:17 +0700

    fix: update cache to v58 for layout and api fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'finance-v57';
+const CACHE = 'finance-v58';
 const FILES = ['./index.html', './style.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
